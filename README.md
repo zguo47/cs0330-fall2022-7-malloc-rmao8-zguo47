a81d136c17aaba41389a2b8f1e925ae8b425c15d
Rx M <rxm@rxdemacbook-pro.devices.brown.edu>
2022-11-30 17:34:38 -0500

    new

--- README.md
+++ README.md
@@ -1,1 +1,34 @@
 # 7-malloc
+Our strategy for maintaining compaction is to introduce the method coalescing(), 
+which given a free block, check to see if the previous block and the next block
+are free blocks; if yes, coalesce the free blocks to be one free block of 
+combined size. In the method mm_free(), when we free a block, we call 
+coalescing() so that if available, the block being freed is automatically 
+combined with other free blocks, which saves the heap from having tiny free 
+blocks. 
+
+Our mm_realloc() considers several cases. If the given pointer is NULL, we 
+directly call mm_malloc(size), if size is equal to 0, we call mm_free(ptr) and 
+return NULL. Then, we compare the size of the original block with the given 
+size, if the new size is smaller than the original size, we return ptr directly,
+and we shorten the memory size to the new size using block_set_size(). Otherwise,
+we check the previous block and the next block; if the previous block is not
+allocated, and the combined size of the two blocks is larger than the requested
+size, we combine the two blocks to a larger block and memmove the original
+block to the new address; if the next block is not allocated, we do the same 
+thing; and if both the previous and the next block aren't allocated, we also
+check the combined size, coalesce and memmove ptr to the new address. In this 
+way we can improve the performance of mm_realloc by utilizing the neighboring
+free blocks if available, avoid calling mem_sbrk() as possible. If there is
+still not enough space, we call mm_malloc with the requested size, memmove ptr
+to the new address and then free ptr. 
+
+Except for the implementation of coalescing() and the strategies in mm_realloc(),
+we also check if the free block size if larger than the requested size, and if
+the extra size is larger than min block size, we set it as a new free block and 
+insert it back to the free list. Only when there is no available free block in
+the free list, we call mem_sbrk() to request new space in heap. 
+
+Our util score for coalesce is around 99, and realloc is around 80. 
+
+Unresolved bugs: N/A
--- mm.c
+++ mm.c
@@ -28,33 +28,39 @@ static inline long align(long size) {
     return (((size) + (WORD_SIZE - 1)) & ~(WORD_SIZE - 1));
 }
 
+/*
+ * coalescing: helper function that helps to coalesce free blocks by merging   *
+ * adjacent smaller free blocks. If the previous or next block of the          *
+ * designated is also free, it will be merged with the block.
+ *
+ * arguments: block_t *block
+ * returns: N/A
+ */
 void coalescing(block_t *block) {
-    
     block_t *next = block_next(block);
     block_t *prev = block_prev(block);
     long new_size;
-    
-    int prev_block_free = 0;
+
     pull_free_block(block);
 
+    // if the previous block is free
     if (!block_allocated(prev)) {
-        
         pull_free_block(prev);
         new_size = block_size(prev) + block_size(block);
         block_set_size_and_allocated(prev, new_size, 0);
-        prev_block_free = 1;
     }
+
+    // if the next block is free
     if (!block_allocated(next)) {
-        
-        if (prev_block_free == 1) {
+        // if both are free
+        if (!block_allocated(prev)) {
             pull_free_block(next);
             new_size = block_size(next) + block_size(prev);
-            
             block_set_size_and_allocated(prev, new_size, 0);
             insert_free_block(prev);
             return;
-
         } else {
+            // if only next block is free
             pull_free_block(next);
             new_size = block_size(next) + block_size(block);
             block_set_size_and_allocated(block, new_size, 0);
@@ -62,14 +68,13 @@ void coalescing(block_t *block) {
             return;
         }
     }
-    
-    if (prev_block_free == 1) {
+
+    if (!block_allocated(prev)) {
         insert_free_block(prev);
     } else {
         insert_free_block(block);
     }
 }
-
 
 /*
  *                             _       _ _
@@ -86,11 +91,13 @@ void coalescing(block_t *block) {
  */
 int mm_init(void) {
     flist_first = NULL;
-    if ((prologue = mem_sbrk(TAGS_SIZE)) == (void *) -1){
+
+    // initiallize and allocate prologue and epilogue
+    if ((prologue = mem_sbrk(TAGS_SIZE)) == (void *)-1) {
         perror("prologue error");
         return -1;
     }
-    if ((epilogue = mem_sbrk(TAGS_SIZE)) == (void *) -1){
+    if ((epilogue = mem_sbrk(TAGS_SIZE)) == (void *)-1) {
         perror("epilogue error");
         return -1;
     }
@@ -112,45 +119,63 @@ int mm_init(void) {
  *          is a multiple of ALIGNMENT), or NULL if an error occurred
  */
 void *mm_malloc(long size) {
-    // TODO
+    // calculate the size required
     long b_size = align(size) + TAGS_SIZE;
-    if (size == 0){
+
+    // if the size is 0, return NULL
+    if (size == 0) {
         return NULL;
     }
-    if (b_size < MINBLOCKSIZE){
+    // if b_size < MINBLOCKSIZE, make it MINBLOCKSIZE. We could also make it
+    // return NULL, but doing so produces lower utility compared to the former
+    if (b_size < MINBLOCKSIZE) {
         b_size = MINBLOCKSIZE;
     }
+
     block_t *curr_block = flist_first;
 
-    while (curr_block != NULL){
-        
-        if (block_size(curr_block) >= b_size){
+    // search through the free list to get the first free block with sufficient
+    // size
+    while (curr_block != NULL) {
+        // if we find free block with sufficient space
+        if (block_size(curr_block) >= b_size) {
             pull_free_block(curr_block);
 
+            // calculate the extra size
             long free_size = block_size(curr_block) - b_size;
 
+            // if the extra size is at least MINBLOCKSIZE, we could split the
+            // rest of the free space into a new free block and insert it into
+            // the free list.
             if (free_size >= MINBLOCKSIZE) {
                 block_set_size_and_allocated(curr_block, b_size, 1);
-                block_set_size_and_allocated(block_next(curr_block), free_size, 0);
-
+                block_set_size_and_allocated(block_next(curr_block), free_size,
+                                             0);
                 insert_free_block(block_next(curr_block));
             } else {
+                // Otherwise, we could ignore the extra size because it is too
+                // small
                 block_set_allocated(curr_block, 1);
             }
-            return curr_block->payload;   
-
-        }else{
-                curr_block = block_flink(curr_block);
-                if (curr_block == flist_first) {
-                    break;
-                }
+            return curr_block->payload;
+        } else {
+            // continue to search through the free list
+            curr_block = block_flink(curr_block);
+            // if we have already traversed the list without finding a suitable
+            // free block, we could exit the while loop
+            if (curr_block == flist_first) {
+                break;
+            }
         }
     }
+    // if there is no free block with sufficient size, we need to extend the
+    // heap to ask for extra free space using mem_sbrk
     block_t *new_block = mem_sbrk(b_size);
     if (new_block == (void *)-1) {
         perror("mem_sbrk error");
         return NULL;
     }
+    // update epilogue after extending the heap
     new_block = epilogue;
     block_set_size_and_allocated(new_block, b_size, 1);
     epilogue = block_next(new_block);
@@ -170,16 +195,18 @@ void *mm_malloc(long size) {
  * returns: nothing
  */
 void mm_free(void *ptr) {
-    // TODO
+    // if ptr is NULL, return directly
     if (ptr == NULL) {
         return;
     }
     block_t *block = payload_to_block(ptr);
+    // if block is already freed
     if (!block_allocated(block)) {
         return;
     }
     block_set_allocated(block, 0);
     insert_free_block(block);
+    // use coalescing helper to increase utility
     coalescing(block);
     return;
 }
@@ -198,71 +225,81 @@ void mm_free(void *ptr) {
  * returns: a pointer to the new memory block's payload
  */
 void *mm_realloc(void *ptr, long size) {
-    // TODO
-    if (ptr == NULL){
+    // if ptr is NULL, call malloc directly
+    if (ptr == NULL) {
         mm_malloc(size);
-    }else if (size == 0){
+    } else if (size == 0) {
+        // if size is 0, free ptr
         mm_free(ptr);
         return NULL;
-    }else{
+    } else {
         block_t *original = payload_to_block(ptr);
         long b_size = align(size) + TAGS_SIZE;
         long old_size = block_size(original);
-        if (old_size >= b_size){
-            long extra_size = old_size - b_size;
-            if (extra_size >= MINBLOCKSIZE){
-                block_set_size(original, b_size);
-                block_set_size_and_allocated(block_next(original), extra_size, 0);
-                insert_free_block(block_next(original));
-                coalescing(block_next(original));
-            }
+        // if the new size is smaller than the original, return ptr directly. We
+        // could implement shortening, however, doing so somehow lower our
+        // utility, so we simply return ptr.
+        if (old_size >= b_size) {
+            // long extra_size = old_size - b_size;
+            // if (extra_size >= MINBLOCKSIZE){
+            //     block_set_size(original, b_size);
+            //     block_set_size_and_allocated(block_next(original), extra_size, 0);
+            //     insert_free_block(block_next(original));
+            //     coalescing(block_next(original));
+            // }
             return ptr;
-        }
-        else{
+        } else {
+            // if the new size is larger
             block_t *next = block_next(original);
             block_t *prev = block_prev(original);
-            if ((!block_allocated(prev)) && (old_size + block_size(prev)) >= b_size) {
-            
+            // if the previous block is free, and the size of the two blocks
+            // together is sufficient, we could combine the two blocks
+            if ((!block_allocated(prev)) &&
+                (old_size + block_size(prev)) >= b_size) {
                 pull_free_block(prev);
-                block_set_size_and_allocated(
-                    prev, old_size + block_size(prev), 1);
+                block_set_size_and_allocated(prev, old_size + block_size(prev),
+                                             1);
                 original = prev;
                 memmove(original->payload, ptr, old_size);
                 return original->payload;
             }
-        
-            else if ((!block_allocated(next)) && (old_size + block_size(next)) >= b_size) {
+            // if the next block is free, and the size of the two blocks
+            // together is sufficient, we could coombine the two block
+            else if ((!block_allocated(next)) &&
+                     (old_size + block_size(next)) >= b_size) {
                 block_t *next = block_next(original);
                 pull_free_block(next);
-                block_set_size_and_allocated(
-                    original, old_size + block_size(next), 1);
+                block_set_size_and_allocated(original,
+                                             old_size + block_size(next), 1);
                 return original->payload;
             }
-
-            else if ((!block_allocated(prev)) && (!block_allocated(next)) && (old_size + block_size(prev) + block_size(next)) >=
-            b_size) {
-
+            // if the size is really large that we need to combine both previous
+            // and next block (given they are free), we could do so
+            else if ((!block_allocated(prev)) && (!block_allocated(next)) &&
+                     (old_size + block_size(prev) + block_size(next)) >=
+                         b_size) {
                 pull_free_block(next);
                 pull_free_block(prev);
                 block_set_size_and_allocated(
-                    prev, old_size + block_size(next) + block_size(prev),
-                    1);
+                    prev, old_size + block_size(next) + block_size(prev), 1);
 
                 original = prev;
                 memmove(original->payload, ptr, old_size);
                 return original->payload;
             }
-        
-           else {
-            void *newptr = mm_malloc(b_size);
-            if (newptr){
-                memmove(newptr, ptr, old_size);
-                mm_free(ptr);
+            // if none of the above are sufficient, we could call malloc, which
+            // first search through the free list and then call memsbrk if there
+            // is no free block large enough in the list
+            else {
+                void *newptr = mm_malloc(b_size);
+                if (newptr) {
+                    memmove(newptr, ptr, old_size);
+                    mm_free(ptr);
+                }
+                return newptr;
             }
-            return newptr;}
         }
     }
 
     return NULL;
-
 }
